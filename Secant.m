36fc0a36460cfdc08bd40d7de61e449f62b9a547
Lorenzofman <lorenzofman@gmail.com>
2019-09-27 09:41:10 -0300

    Add secant file for delivering

--- Secant.m
+++ Secant.m
@@ -1,12 +1,11 @@
-function Secant(f, x, x0, e)
+function Secante_21(f, x, x0, erro)
   i = 0;
-  h = e;
-  format long
+  h = erro;
   do
     i += 1;
-    diffSecant = (x0 * f(x) - x * f(x0))/(f(x) - f(x0))
-    x0 = x
+    diffSecant = (x0 * f(x) - x * f(x0))/(f(x) - f(x0));
+    x0 = x;
     x = diffSecant;
-  until (abs(f(x)) < e && abs(x - x0) < e)
-  printf("Using %i iterations, the result is %f\n", i, x)
+  until (abs(f(x)) < erro && abs(x - x0) < erro)
+  printf("Using %i iterations, the result is %.15f\n", i, x)
 endfunction
